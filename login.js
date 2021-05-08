aaf90ef39a91a390908eefdc753e220fffdb7496
Puiu Ionut <puiuionut.cristian@gmail.com>
2021-05-08 23:30:06 +0300

    add user page+doctor/patient page+some design

--- login.js
+++ login.js
@@ -2,18 +2,28 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
       
-      document.getElementById("user_div").style.display = "block";
       document.getElementById("login_div").style.display = "none";
   
       document.getElementById("loggedin-header").style.display = "block";
       document.getElementById("login-header").style.display = "none";
+      document.getElementById("doctor_add").style.display = "none";
+      
+
       var user = firebase.auth().currentUser;
   
       if(user != null){
   
         var email_id = user.email;
-        document.getElementById("user_para").innerHTML = "Bine ai venit : " + email_id;
-  
+        document.getElementById("user_doctor").innerHTML = "Bine ai venit : " + email_id;
+        if(email_id=="polverea1998@yahoo.com"){
+          document.getElementById("user_patient_div").style.display = "block";
+          document.getElementById("user_doctor_div").style.display = "none";
+        }
+        if(email_id=="ionutpuiu@yahoo.com"){
+          document.getElementById("user_patient_div").style.display = "none";
+          document.getElementById("user_doctor_div").style.display = "block";
+        }
+        
       }
   
     } else {
@@ -21,8 +31,10 @@ firebase.auth().onAuthStateChanged(function(user) {
       document.getElementById("loggedin-header").style.display = "none";
       document.getElementById("login-header").style.display = "block";
 
-      document.getElementById("user_div").style.display = "none";
       document.getElementById("login_div").style.display = "block";
+      document.getElementById("user_patient_div").style.display = "none";
+      document.getElementById("user_doctor_div").style.display = "none";
+      document.getElementById("doctor_add").style.display = "none";
   
     }
   });
@@ -31,19 +43,28 @@ firebase.auth().onAuthStateChanged(function(user) {
   
     var userEmail = document.getElementById("email_field").value;
     var userPass = document.getElementById("password_field").value;
-  
+
     firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-  
-      window.alert("Error : " + errorMessage);
-  
-      // ...
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
     });
   
   }
   
+  function add_patient(){
+    document.getElementById("user_doctor_div").style.display = "none";
+    document.getElementById("doctor_add").style.display = "block";
+  }
+
+  function cancel_add(){
+    document.getElementById("user_doctor_div").style.display = "block";
+    document.getElementById("doctor_add").style.display = "none";
+  }
   function logout(){
     firebase.auth().signOut();
   }
