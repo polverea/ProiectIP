fa6e31b004c6ca0f4320d4b12a32a3e70bf4e1c1
Marius Polverea <polvereamarius@yahoo.com>
2021-05-08 17:50:37 +0300

    Logo + buttons

--- login.js
+++ login.js
@@ -1,22 +1,26 @@
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
-  
+      
       document.getElementById("user_div").style.display = "block";
       document.getElementById("login_div").style.display = "none";
   
+      document.getElementById("loggedin-header").style.display = "block";
+      document.getElementById("login-header").style.display = "none";
       var user = firebase.auth().currentUser;
   
       if(user != null){
   
         var email_id = user.email;
-        document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+        document.getElementById("user_para").innerHTML = "Bine ai venit : " + email_id;
   
       }
   
     } else {
       // No user is signed in.
-  
+      document.getElementById("loggedin-header").style.display = "none";
+      document.getElementById("login-header").style.display = "block";
+
       document.getElementById("user_div").style.display = "none";
       document.getElementById("login_div").style.display = "block";
   
