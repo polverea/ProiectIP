5d770041fd9870fd4bc5fd13e5b906d22b2dce26
Marius Polverea <polvereamarius@yahoo.com>
2021-05-11 23:41:40 +0300

    Patients view + eliminare sign up

--- add.js
+++ add.js
@@ -5,6 +5,7 @@ const nume=document.getElementById("name_patient_field");
 const prenume=document.getElementById("surname_patient_field");
 const CNP=document.getElementById("cnp_patient_field");
 const adresa=document.getElementById("adress_patient");
+const telefon=document.getElementById("phone_number_field");
 const job=document.getElementById("job_field");
 const istoric=document.getElementById("medical_history_field");
 const alergii=document.getElementById("allergies_field");
@@ -15,26 +16,17 @@ const addButton=document.getElementById("add_button");
 const database=firebase.firestore();
 const pacientiCollection = database.collection("pacienti");
 
-function sign_up(){
 
-    firebase.auth().createUserWithEmailAndPassword(username.value, parola.value);
-    firebase.auth().signOut().then(() => {
-    //chestii
-    }).catch((error) => {
-    // An error happened.
-    });
-}
 
 addButton.addEventListener("click",e => {
     e.preventDefault();
-    sign_up();
     pacientiCollection.doc(username.value).set(
         {
             name_patient_field: nume.value,
-            password_patient_field: parola.value,
-            name_patient_field: prenume.value,
+            surname_patient_field: prenume.value,
             cnp_patient_field: CNP.value,
             adress_patient:adresa.value,
+            phone_number_field: telefon.value,
             job_field:job.value,
             medical_history_field:istoric.value,
             allergies_field:alergii.value,
--- login.js
+++ login.js
@@ -6,7 +6,8 @@ firebase.auth().onAuthStateChanged(function(user) {
       document.getElementById("loggedin-header").style.display = "block";
       document.getElementById("login-header").style.display = "none";
       document.getElementById("doctor_add").style.display = "none";
-      document.getElementById("menu-span").style.display = "none";      
+      document.getElementById("menu-span").style.display = "none"; 
+      document.getElementById("info_pacient").style.display = "none";       
 
       var user = firebase.auth().currentUser;
   
@@ -18,11 +19,13 @@ firebase.auth().onAuthStateChanged(function(user) {
           document.getElementById("user_patient_div").style.display = "block";
           document.getElementById("user_doctor_div").style.display = "none";
           document.getElementById("menu-span").style.display = "none";
+          document.getElementById("info_pacient").style.display = "none";   
         }
         if(email_id=="ionutpuiu@yahoo.com"){
           document.getElementById("user_patient_div").style.display = "none";
           document.getElementById("user_doctor_div").style.display = "block";
           document.getElementById("menu-span").style.display = "none";
+          document.getElementById("info_pacient").style.display = "none";   
         }
       }
   
@@ -36,6 +39,7 @@ firebase.auth().onAuthStateChanged(function(user) {
       document.getElementById("user_doctor_div").style.display = "none";
       document.getElementById("doctor_add").style.display = "none";
       document.getElementById("menu-span").style.display = "none";
+      document.getElementById("info_pacient").style.display = "none";   
   
     }
   });
@@ -61,12 +65,14 @@ firebase.auth().onAuthStateChanged(function(user) {
     document.getElementById("user_doctor_div").style.display = "none";
     document.getElementById("doctor_add").style.display = "block";
     document.getElementById("menu-span").style.display = "block";
+    document.getElementById("info_pacient").style.display = "none";   
   }
 
   function cancel_add(){
     document.getElementById("user_doctor_div").style.display = "block";
     document.getElementById("doctor_add").style.display = "none";
     document.getElementById("menu-span").style.display = "none";
+    document.getElementById("info_pacient").style.display = "none";   
   }
 
   function add()
@@ -88,6 +94,29 @@ firebase.auth().onAuthStateChanged(function(user) {
         }).catch(function (error){
             console.log("error ", error);
         });
+    })
+  }
+  const db=firebase.firestore();
+  const usersCollection = db.collection('pacienti');
+  
+  
+  function view_patient(){
+    document.getElementById("user_doctor_div").style.display = "none";
+    document.getElementById("doctor_add").style.display = "none";
+    document.getElementById("menu-span").style.display = "block";
+    document.getElementById("info_pacient").style.display = "block";
+    var table = document.getElementById("myTable");
+    usersCollection.get()
+    .then(snapshot =>{
+      snapshot.forEach(user => {
+        var row = table.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML =  user.id;
+        cell2.innerHTML = user.data().name_patient_field; 
+        cell3.innerHTML = user.data().cnp_patient_field;     
+      });
     })
   }
 
